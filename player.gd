dba06099104d28f306f81ec31d9a552921650b9b
D-eyniel <melvinjrpacete6@gmail.com>
2025-05-29 13:24:53 +0800

    update

--- player.gd
+++ player.gd
@@ -50,5 +50,4 @@ func start(pos):
 func _on_body_entered(_body):
 	hide() # Player disappears after being hit.
 	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred(&"disabled", true)
